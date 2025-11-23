f9279952a8d71dc3f68adb4b18bcd57b05d8d896
Isaac <isaacngrwi@gmail.com>
2025-11-23 09:56:23 +0000

    Implemented feature to have config.py pull secret_key from .env

--- config.py
+++ config.py
@@ -1,5 +1,11 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
 class Config:
     DEBUG = True
-    SECRET_KEY = 'supersecretkey'
+    SECRET_KEY = os.getenv("SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+# config_test = Config()
+# print(config_test.SECRET_KEY)
