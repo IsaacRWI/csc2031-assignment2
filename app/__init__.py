49b99f95b9c5af68e374050fc205351629487ca2
Isaac <isaacngrwi@gmail.com>
2025-11-30 16:26:37 +0000

    Initialized flask extensions for later user except for csrf protection

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,26 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
+from flask_wtf import CSRFProtect
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
+# initialize flask extensions for later use
 db = SQLAlchemy()
+csrf = CSRFProtect()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
     db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+
+    login_manager.login_view = "main.login"
+    login_manager.login_message_category = "info"
 
     from .routes import main
     app.register_blueprint(main)
