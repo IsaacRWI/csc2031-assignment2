58ba67b67319f03fa28a756068b1ce3e7b0fd48c
Isaac <isaacngrwi@gmail.com>
2025-12-06 02:26:26 +0000

    Implemented secure cookie flags

--- config.py
+++ config.py
@@ -6,6 +6,9 @@ class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SESSION_COOKIE_HTTP_ONLY = True
+    SESSION_COOKIE_SECURE = True
+    SESSION_COOKIE_SAMESITE = "Lax"
 
 # config_test = Config()
 # print(config_test.SECRET_KEY)
