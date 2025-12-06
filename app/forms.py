2720ece1f272f57ed33571c0cf8564295daacbfb
Isaac <isaacngrwi@gmail.com>
2025-12-06 00:51:09 +0000

    Added email validator to login as login must be a valid email address

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, TextAreaField, SubmitField
-from wtforms.validators import DataRequired, EqualTo, Length, ValidationError
+from wtforms.validators import DataRequired, EqualTo, Length, ValidationError, Email
 
 class LoginForm(FlaskForm):
-    username = StringField("username", validators=[DataRequired()])
+    username = StringField("username", validators=[DataRequired(), Email()])
     password = PasswordField("password", validators=[DataRequired()])
     submit = SubmitField("login")
