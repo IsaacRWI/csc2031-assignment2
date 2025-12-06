975b3d43436cb72806becf9f8fef744a7353bfe3
Isaac <isaacngrwi@gmail.com>
2025-12-06 03:53:05 +0000

    Added username validator to fix error where registering with an email already in the database would brick the page

--- app/forms.py
+++ app/forms.py
@@ -3,6 +3,7 @@ from wtforms import StringField, PasswordField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired, EqualTo, Length, ValidationError, Email
 import re
 from bleach import clean
+from app.models import User
 
 common_passwords = {"Password123$", "Qwerty123!", "Adminadmin1@", "weLcome123!", "CustomPassword1234!", "loGinPasssWORD13213$"}
 safe_tags = {"b", "i", "u", "em", "strong", 'a', 'p', 'ul', 'ol', 'li', 'br'}
@@ -19,6 +20,12 @@ class RegisterForm(FlaskForm):
     confirm_password = PasswordField("Confirm password", validators=[DataRequired(), EqualTo("password", message="Must match original password")])
     bio = TextAreaField("Enter something about yourself")
     submit = SubmitField("Register")
+
+    def validate_username(self, username):
+        username = username.data
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user:
+            raise ValidationError("Username already exists in database")
 
     def validate_password(self, password):
         password = password.data
