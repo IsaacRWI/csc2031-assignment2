e9eeb987135388f3de2b955807cf82d0f79ae081
Isaac <isaacngrwi@gmail.com>
2025-12-08 21:41:44 +0000

    Added comments to logger.py

--- app/logger.py
+++ app/logger.py
@@ -2,6 +2,10 @@ from flask import request, current_app
 from datetime import datetime
 
 def log_event(level, message, username=None):
+    """
+    logger function to define how log events works
+    had to move it to a separate file to avoid a circular import error or smth like that
+    """
     ip = request.remote_addr or "N/A"
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     log_message = f"[{level}] {timestamp} Client IP: {ip}, User: {username or "N/A"} | {message}"
